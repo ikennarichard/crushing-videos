d72ef2b9cd72cf706d7e958412966a8d5c9b77db
Ikenna Richard <oguejioforrichard@gmail.com>
2026-03-26 08:48:47 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-neutral-950 text-white flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Crushing Videos
+      </h1>
+    </div>
+  );
+}
